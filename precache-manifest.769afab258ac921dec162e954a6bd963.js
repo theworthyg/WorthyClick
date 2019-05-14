1cd32cb0e7d55a1dd13e215449529dac2b851462
theWorthyG <wdg2001@wildcats.unh.edu>
2019-05-13 20:41:31 -0400

    Updates

---
+++ precache-manifest.769afab258ac921dec162e954a6bd963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1acc478e12ffe663bbd7ee12c0277371",
+    "url": "/worthyclick/index.html"
+  },
+  {
+    "revision": "61f7b6593b4e6ca4c838",
+    "url": "/worthyclick/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/worthyclick/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "61f7b6593b4e6ca4c838",
+    "url": "/worthyclick/static/js/main.f5433f34.chunk.js"
+  },
+  {
+    "revision": "97d69bc7c508d570dc30",
+    "url": "/worthyclick/static/js/runtime~main.0f48ef45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/worthyclick/static/media/logo.5d5d9eef.svg"
+  }
+]);
